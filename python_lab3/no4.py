08cfd4403e4108a31627d803dc2c758b7c6fb6af
Thura Aung <thuraaung.ai.mdy@gmail.com>
2023-07-25 17:44:23 +0700

    Update no4.py

--- python_lab3/no4.py
+++ python_lab3/no4.py
@@ -5,7 +5,8 @@ ascii_hex = format(ascii,"x")
 
 if ascii_hex >= '30' and ascii_hex <= '39':
     print("{} is a numeric character.".format(char))
-elif ascii >= 0x41 and ascii <= 0x5a:
+#elif ascii >= 0x41 and ascii <= 0x5a:
+elif ascii_hex >= '41' and ascii_hex <= '5a':
     print("{} is a capital letter and its small-case letter is {}".format(char,char.lower()))
 elif ascii_hex >= '61' and ascii_hex <= '7a':
     print("{} is a small-case letter and its capital letter is {}".format(char,char.upper()))
