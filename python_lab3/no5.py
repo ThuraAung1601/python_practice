ef817451ca3f71f9880f2689779cf12f30264870
Thura Aung aka June <thuraaung.ai.mdy@gmail.com>
2023-08-08 18:32:04 +0700

    Update no5.py

--- python_lab3/no5.py
+++ python_lab3/no5.py
@@ -1,16 +1,20 @@
 char = input("Please enter a character:")
-ascii = ord(char)
+char_list = [ c for c in char ]
+
+def check_it(ascii):
+    if ascii >= 0x30 and ascii <= 0x39:
+        print("{} is a numeric character.".format(c))
+    elif ascii >= 0x41 and ascii <= 0x5a:
+        print("{} is a capital letter and its small-case letter is {}".format(c,c.lower()))
+    elif ascii >= 0x61 and ascii <= 0x7a:
+        print("{} is a small-case letter and its capital letter is {}".format(c,c.upper()))
+    else:
+        print("{} is a special character.".format(c))
 
 while char != "\t":
-    if ascii >= 0x30 and ascii <= 0x39:
-        print("{} is a numeric character.".format(char))
-    elif ascii >= 0x41 and ascii <= 0x5a:
-        print("{} is a capital letter and its small-case letter is {}".format(char,char.lower()))
-    elif ascii >= 0x61 and ascii <= 0x7a:
-        print("{} is a small-case letter and its capital letter is {}".format(char,char.upper()))
-    else:
-        print("{} is a special character.".format(char))
+    for c in char_list:
+        check_it(ord(c))
     char = input("Please enter a character:")
-    ascii = ord(char)
+    char_list = [ c for c in char ]
 
 print("See You Tomorrow")
