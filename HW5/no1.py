26df31bd9381483b883e7992791ac57cd89deb33
June <thuraaung.ai.mdy@gmail.com>
2023-08-14 12:37:57 +0700

    added question to no1.py

--- HW5/no1.py
+++ HW5/no1.py
@@ -1,3 +1,7 @@
+'''
+Write a Python program to iterate the step 4-5 calculation of Newton's number guessing method with 5, 6 and 7 time respectively 
+in order to compare the approximation results and report your answers in the three decimal points of accuracy.
+'''
 n = float(input("Enter a number to find its square root:"))
 guess = n/2
 for i in range(1,8):
