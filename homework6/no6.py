59ca388ca85f70fef7f6f4e858b4536a5dc9b781
June <thuraaung.ai.mdy@gmail.com>
2023-08-20 23:58:20 +0700

    Delete homework6 directory

--- homework6/no6.py
+++
@@ -1,8 +0,0 @@
-num = input("Enter a number:")
-def rev_int(num):
-    num_list = [c for c in num]
-    rev_num_list = num_list[::-1]
-    result_str = "".join(rev_num_list)
-    result = int(result_str)
-    return result
-print(rev_int(num))
