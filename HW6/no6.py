2da536c937c6ac26d089c432872d235db09e92ba
June <thuraaung.ai.mdy@gmail.com>
2023-08-21 00:01:43 +0700

    Update no6.py

--- HW6/no6.py
+++ HW6/no6.py
@@ -1,8 +1,17 @@
+'''
+No.6 
+Write a function to return an integer 
+whose digits are in the reversed orders of the given integer.
+For example, reverse(3456) returns 6543
+'''
+
 def reverse(num):
-    result_str = num[::-1]
+    result_str = str(num)[::-1]
     result = int(result_str)
     return result
 
 def main():
-    num = input("Enter a number:")
+    num = int(input("Enter a number:"))
     print(reverse(num))
+
+main()
