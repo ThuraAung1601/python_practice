03a98f1a88b959f4a472f101f4fd6d929e38a7c7
June <thuraaung.ai.mdy@gmail.com>
2023-08-26 15:51:43 +0700

    Update num_sequence.py

--- For_midterm/num_sequence.py
+++ For_midterm/num_sequence.py
@@ -2,3 +2,11 @@ for i in range(50, 0, -1):
     if i%3 == 0 or i%5 == 0: continue
     elif i != 1: print(i, end=",")
     else: print(i, end=".\n")
+
+# result = ""
+# for i in range(50, 0, -1):
+#     if i % 3 == 0 or i % 5 == 0:
+#         continue
+#     result += f"{i}, " if i != 1 else f"{i}.\n"
+
+# print(result)
