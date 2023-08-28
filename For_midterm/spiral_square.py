0ee61e0df097ce8654997e29a25c3d1e2583b83d
June <thuraaung.ai.mdy@gmail.com>
2023-08-28 21:47:51 +0700

    Update spiral_square.py

--- For_midterm/spiral_square.py
+++ For_midterm/spiral_square.py
@@ -1,42 +1,35 @@
 import turtle
 
-# Function to draw a square of size n
-def draw_square(n):
-    for _ in range(4):
-        turtle.forward(n)
-        turtle.right(90)
+def draw_sq(size):
+    turtle.penup()
+    turtle.forward(size/2)
+    turtle.pendown()
+    turtle.left(90)
+    turtle.forward(size/2)
+    turtle.left(90)
+    turtle.forward(size)
+    turtle.left(90)
+    turtle.forward(size)
+    turtle.left(90)
+    turtle.forward(size)
+    turtle.left(90)
+    turtle.forward(size/2)
 
-# Function to draw nested squares with a gap
-def draw_nested_squares(s, g):
-    while s >= 20:
-        draw_square(s)
+def draw_spiral(n):
+    degree = 10
+    draw_sq(n)
+
+    while n > 5:
+        # go back to center
         turtle.penup()
-        # Adjust starting position based on initial square size
-        turtle.goto(turtle.xcor()+g, turtle.ycor()-g)  
-        turtle.pendown() 
-        # Reduce the size for the next inner square 
-        s -= 2 * g  
+        turtle.goto(0,0)
+        turtle.pendown()
 
-# Function to draw nested squares with a gap
-def draw_nested_spiral_squares(s, g):
-    draw_square(s)
-    s = s - (2 * g)
-    while s >= 5:
-        turtle.penup()
-        # Adjust starting position based on initial square size
-        turtle.goto(turtle.xcor()+g, turtle.ycor()-g) 
-        # Rotate the square
-        turtle.left(10) 
-        turtle.pendown() 
-        draw_square(s)
-        # Reduce the size for the next inner square 
-        s = s - (2 * g) 
+        # update n size
+        n = n * 0.75
 
-# Size of the outermost square
-initial_size = 150
-# Gap between inner squares  
-gap = 20
+        # rotate for spiral
+        turtle.left(degree)
+        draw_sq(n)
 
-draw_nested_squares(initial_size, gap)  # Drawing the nested squares
-# draw_nested_spiral_squares(initial_size, gap)
-turtle.done()
+draw_spiral(150)
