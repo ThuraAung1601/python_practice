538d21424fb5863538d9c74e7a95ac374285380b
June <thuraaung.ai.mdy@gmail.com>
2023-09-26 16:45:30 +0700

    Update no3.py

--- lab10/no3.py
+++ lab10/no3.py
@@ -2,3 +2,13 @@ list1 = [3, 1, 1, 1, 2, 7]
 list2 = [4, 1, 1, 2, 2, 5]
 
 def get_the_difference(list1, list2):
+    result = []
+    for i in list1:
+        if i not in list2:
+            result.append(i)
+    for j in list2:
+        if j not in list1:
+            result.append(j)
+    print(result)
+
+get_the_difference(list1, list2)
