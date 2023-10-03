68821431ce55f71c3e8b7076df623321f5b5a94e
June <thuraaung.ai.mdy@gmail.com>
2023-10-03 09:03:15 +0000

    Update turtle_shapes.py

--- turtle_shapes.py
+++ turtle_shapes.py
@@ -37,3 +37,38 @@ def draw_rectangle(coor1, coor2):
 
 # draw_rectangle((10, 10), (23, 34))
 # turtle.done()
+
+def star(length):
+    inner_degree = 36
+    outer_degree = 180 - 36
+    turtle.showturtle()
+    for i in range(5):
+        turtle.forward(length)
+        turtle.right(outer_degree)
+        turtle.forward(length)
+
+# star(30)
+# turtle.done()
+
+
+def triangle(coor1, coor2, coor3):
+    x1, y1 = coor1[0], coor1[1]
+    x2, y2 = coor2[0], coor2[1]
+    x3, y3 = coor3[0], coor3[1]
+    # Side 1
+    turtle.penup()
+    turtle.goto(x1,y1)
+    turtle.write((x1,y1))
+    turtle.pendown()
+    # Side 2
+    turtle.goto(x2,y2)
+    turtle.write((x2,y2))
+    # Side 3
+    turtle.goto(x3,y3)
+    turtle.write((x3,y3))
+
+    turtle.goto(x1,y1)
+    turtle.pendown()
+
+# triangle((1,1), (30, 12), (20, 20))
+# turtle.done()
