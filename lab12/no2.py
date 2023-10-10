119aca45117ac71ad07233e03b8bf3a0bd4f891d
June <thuraaung.ai.mdy@gmail.com>
2023-10-10 12:29:10 +0000

    Update no2.py

--- lab12/no2.py
+++ lab12/no2.py
@@ -11,6 +11,10 @@ def find_duplicates(dict):
     name_list = list(dict.values())
 
     for key, value in dict.items():
+        # if it is duplicated, the value will be more than 1 time
+        # try the following in the terminal/ Python shell
+        # ls = [1, 2, 1, 3, 4]
+        # ls.count(1) # output will be 2 cuz there are two 1s in the list ls
         if name_list.count(value) > 1:
             duplicated[key] = value
 
