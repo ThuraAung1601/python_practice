aff95d9c0583ecaf70ce89fce63ed97359911dd0
June <thuraaung.ai.mdy@gmail.com>
2023-10-13 19:08:58 +0000

    Update no5.py

--- lab12/no5.py
+++ lab12/no5.py
@@ -1,11 +1,24 @@
-s = set([1, 2, 3])
+def power_set(i):
+    i_list = list(i)
+    n = len(i_list)
 
-def power(s):
-    if len(s) == 0:
-        return {frozenset()}
-    else:
-        x = s.pop()
-        p = power(s)
-        return p.union({e.union({x}) for e in p})
+    # Calculate the total number of subsets (2^n)
+    total_subsets = 2 ** n
 
-print(power(s))
+    # Generate all subsets using binary representation
+    all_subsets = []
+    for subset_index in range(total_subsets):
+        subset = set()
+        for j in range(n):
+            # Check if the j-th bit of subset_index is set (1)
+            if (subset_index >> j) & 1:
+                subset.add(i_list[j])
+        all_subsets.append(frozenset(subset))
+
+    return all_subsets
+
+# Example usage:
+i = frozenset({1, 2, 3})
+result = power_set(i)
+for subset in result:
+    print(subset)
