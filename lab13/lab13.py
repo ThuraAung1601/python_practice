c1afb7cb5fab041631222346a6be3f016b278d27
June <thuraaung.ai.mdy@gmail.com>
2023-10-17 08:24:51 +0000

    Update lab13.py

--- lab13/lab13.py
+++ lab13/lab13.py
@@ -60,12 +60,13 @@
 # import turtle
 # import random
 
+# turtle.speed(0)
 # turtle.Turtle()
 
 # def cross(width, times):
 #     if times == 0:
 #         hexadecimal = "#" + ''.join([random.choice("ABCDEF0123456789") for i in range(6)])
-#         turtle.dot(30, hexadecimal)
+#         turtle.dot(3, hexadecimal)
 #         return 
 #     else:
 #         for i in range(4):
@@ -75,7 +76,7 @@
 #             turtle.forward(width)
 #             turtle.left(90)
 
-# cross(100, 7)
+# cross(100, 4)
 # turtle.update()
 # turtle.done()
 
