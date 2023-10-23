56edff0c99f0c40843d9cf1324b3cf039d045225
June <thuraaung.ai.mdy@gmail.com>
2023-10-23 17:55:37 +0700

    Update no5.py

--- homework13/no5.py
+++ homework13/no5.py
@@ -3,39 +3,52 @@ No.5
 Write a program to display a recursive tree.
 '''
 import math
-from tkinter import * 
+from tkinter import *  # Import tkinter
 
-def display():
-    canvas.delete("line")
-    displayRTree(int(order.get()), width/2, height-100, height / 3, math.pi / 2)
 
-def displayRTree(depth, x1, y1, length, angle):
-    if depth >= 0:
-        x2 = x1 - math.cos(angle) * length
-        y2 = y1 - math.sin(angle) * length
-        # Draw the line
-        canvas.create_line(x1, y1, x2, y2, tag="line")
-        # Draw the left branch
-        displayRTree(depth - 1, x2, y2, length * sizeFactor, angle + angleFactor)
-        # Draw the right branch
-        displayRTree(depth - 1, x2, y2, length * sizeFactor, angle - angleFactor)
+class Recursive_Tree:
+    def __init__(self):
+        window = Tk()  # Create a window
+        window.title("Recursive tree")  # Set a title
 
-window = Tk()
-window.title("Recursive tree")
+        self.width = 600
+        self.height = 600
+        self.canvas = Canvas(window,
+                             width=self.width, height=self.height)
+        self.canvas.pack()
 
-width = 600
-height = 600
-canvas = Canvas(window, width=width, height=height)
-canvas.pack()
+        # Add a label, an entry, and a button to frame1
+        frame1 = Frame(window)  # Create and add a frame to window
+        frame1.pack()
+        self.angleFactor = math.pi / 5
+        self.sizeFactor = 0.58
+        Label(frame1,
+              text="Enter an order: ").pack(side=LEFT)
+        self.order = StringVar()
 
-frame1 = Frame(window) 
-frame1.pack()
-angleFactor = math.pi / 5
-sizeFactor = 0.6
-Label(frame1, text="Enter an order: ").pack(side=LEFT)
-order = StringVar()
+        entry = Entry(frame1, textvariable=self.order,
+                      justify=RIGHT).pack(side=LEFT)
+        Button(frame1, text="Display Recursive tree",
+               command=self.display).pack(side=LEFT)
 
-entry = Entry(frame1, textvariable=order, justify=RIGHT).pack(side=LEFT)
-Button(frame1, text="Display Recursive tree", command=display).pack(side=LEFT)
+        window.mainloop()  # Create an event loop
 
-window.mainloop() 
+    def display(self):
+        self.canvas.delete("line")
+        self.displayRTree(int(self.order.get()), self.width/2, self.height-100, self.height / 3, math.pi / 2)
+
+    def displayRTree(self, depth, x1, y1, length, angle):
+        if depth >= 0:
+            x2 = x1 - math.cos(angle) * length
+            y2 = y1 - math.sin(angle) * length
+
+            # Draw the line
+            self.canvas.create_line(x1, y1, x2, y2, tag="line")
+
+            # Draw the left branch
+            self.displayRTree(depth - 1, x2, y2, length * self.sizeFactor, angle + self.angleFactor)
+            # Draw the right branch
+            self.displayRTree(depth - 1, x2, y2, length * self.sizeFactor, angle - self.angleFactor)
+
+
+Recursive_Tree()  # Create GUI
