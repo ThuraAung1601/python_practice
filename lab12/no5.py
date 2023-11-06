fc7ceb5e0d964e029b0af399e3798fd629b27d5b
June <thuraaung.ai.mdy@gmail.com>
2023-11-06 11:02:24 +0700

    Update no5.py

--- lab12/no5.py
+++ lab12/no5.py
@@ -1,24 +1,38 @@
-def power_set(i):
-    i_list = list(i)
-    n = len(i_list)
+set1 = frozenset({1, 2, 3})
+ls = list(set1)
+subsets = [frozenset({})]
 
-    # Calculate the total number of subsets (2^n)
-    total_subsets = 2 ** n
+for j in ls:
+    subsets.append(frozenset({j}))
+    for k in ls:
+        subsets.append(frozenset({j, k}))
 
-    # Generate all subsets using binary representation
-    all_subsets = []
-    for subset_index in range(total_subsets):
-        subset = set()
-        for j in range(n):
-            # Check if the j-th bit of subset_index is set (1)
-            if (subset_index >> j) & 1:
-                subset.add(i_list[j])
-        all_subsets.append(frozenset(subset))
+subsets.append(set1)
+print(len(frozenset(subsets)))
+for i in frozenset(subsets):
+    print(i)
+    
+# def power_set(i):
+#     i_list = list(i)
+#     n = len(i_list)
 
-    return all_subsets
+#     # Calculate the total number of subsets (2^n)
+#     total_subsets = 2 ** n
 
-# Example usage:
-i = frozenset({1, 2, 3})
-result = power_set(i)
-for subset in result:
-    print(subset)
+#     # Generate all subsets using binary representation
+#     all_subsets = []
+#     for subset_index in range(total_subsets):
+#         subset = set()
+#         for j in range(n):
+#             # Check if the j-th bit of subset_index is set (1)
+#             if (subset_index >> j) & 1:
+#                 subset.add(i_list[j])
+#         all_subsets.append(frozenset(subset))
+
+#     return all_subsets
+
+# # Example usage:
+# i = frozenset({1, 2, 3})
+# result = power_set(i)
+# for subset in result:
+#     print(subset)
